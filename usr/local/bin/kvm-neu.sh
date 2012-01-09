a2673240a1e4d6b3e404b77692fac47bd4b9af28
Adrian Reyer <are@lihas.de>
2012-01-09 16:20:57 +0100

    Initial release

---
+++ usr/local/bin/kvm-neu.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+[ -e /etc/cluster-tools-lihas.conf ] && . /etc/cluster-tools-lihas.conf
+
+if [ ga$2 == ga ]; then
+  echo "usage: $0 KVMNAME SIZE [DRBDNUM]" >&2
+  echo "  z.B. $0 test01 30G" >&2
+  echo "  z.B. $0 test01 30G 3" >&2
+  exit 1
+fi
+
+KVMNAME=$1
+SIZE=$2
+VIRSHINSTANCES="qemu:///system qemu+ssh://$HOST2/system"
+
+if [ ga$3 == ga ]; then
+  # Naechstes freies DRBD
+  DRBD=$(($(awk '$1 ~ /^device$/ && $2 ~ /^\/dev\/drbd/ {gsub(";","",$2); gsub("/dev/drbd","",$2); print $2}' /etc/drbd.d/*.res | sort -un| tail -1)+1))
+else
+  DRBD=$3
+fi
+
+DRBDPORT=$(printf "%02i" $DRBD)
+
+DUMMY=10
+
+lvcreate -L$SIZE -n kvm_$KVMNAME $VG1
+ssh $HOST2 lvcreate -L$SIZE -n kvm_$KVMNAME $VG2
+
+cat <<-EOF > /etc/drbd.d/kvm_$KVMNAME.res
+resource kvm_$KVMNAME {  
+        protocol        C;
+        syncer {
+                rate    2000M;
+        }
+	startup {
+		become-primary-on both;
+	}
+	net {
+		allow-two-primaries;
+		after-sb-0pri discard-zero-changes;
+		after-sb-1pri discard-secondary;
+		after-sb-2pri disconnect;
+	}
+        on $HOST1 {
+                device          /dev/drbd$DRBD;
+                disk            /dev/$VG1/kvm_$KVMNAME;
+                flexible-meta-disk      internal;
+                address         $IP_DRBD1:77$DRBDPORT;
+        }
+        on $HOST2 {
+                device          /dev/drbd$DRBD;
+                disk            /dev/vg_$HOST2/kvm_$KVMNAME;
+                flexible-meta-disk      internal;
+                address         $IP_DRBD2:77$DRBDPORT;
+        }
+}
+EOF
+
+rsync -rlHpogDtSvx /etc/drbd.d $HOST2:/etc/
+yes yes | drbdadm create-md kvm_$KVMNAME
+drbdadm up kvm_$KVMNAME
+ssh $HOST2 "(yes yes | drbdadm create-md kvm_$KVMNAME && drbdadm up kvm_$KVMNAME)"
+
+drbdadm -- -o primary kvm_$KVMNAME
+parted /dev/drbd$DRBD mklabel msdos
+
+cat <<EOF | crm configure
+primitive p_$KVMNAME ocf:linbit:drbd \
+        params drbd_resource="kvm_$KVMNAME" \
+        op start interval="0" timeout="240" \
+        op promote interval="0" timeout="90" \
+        op demote interval="0" timeout="90" \
+        op stop interval="0" timeout="100" \
+        op monitor interval="10" timeout="20" start-delay="0" \
+        op notify interval="0" timeout="90" \
+        meta target-role="started"
+ms ms_$KVMNAME p_$KVMNAME \
+        meta master-max="2" clone-max="2" notify="true" interleave="true"
+commit
+EOF
+
+while ! drbd-overview | sed 's/:/ /g' | awk '$2 ~ /^kvm_'$KVMNAME'$/' | grep -q Primary/Primary; do
+  cat /proc/drbd
+  sleep 5
+done
+
+virsh pool-create-as --name $KVMNAME --type disk --source-path dev --source-dev /dev/drbd$DRBD --target /dev/
+virsh pool-dumpxml $KVMNAME > /etc/libvirt/storage/$KVMNAME.xml
+ln -s /etc/libvirt/storage/$KVMNAME.xml /etc/libvirt/storage/autostart/$KVMNAME.xml
+ssh $HOST2 /etc/init.d/libvirt-bin stop
+rsync -rlHpogDtSvx --numeric-ids /etc/libvirt $HOST2:/etc/
+ssh $HOST2 /etc/init.d/libvirt-bin start
+/etc/init.d/libvirt-bin restart
+
+DOMFILE=`mktemp`
+cat <<-EOF >$DOMFILE
+<domain type='kvm' id='3'>
+  <name>$KVMNAME</name>
+  <uuid>e48d949d-ea81-4ad8-a2b4-56127cd392$DRBDPORT</uuid>
+  <memory>524288</memory>
+  <currentMemory>524288</currentMemory>
+  <vcpu>1</vcpu>
+  <os>
+    <type arch='x86_64' machine='pc-0.12'>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+    <pae/>
+  </features>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>restart</on_crash>
+  <devices>
+    <emulator>/usr/bin/kvm</emulator>
+    <disk type='block' device='disk'>
+      <driver name='qemu' type='raw'/>
+      <source dev='/dev/drbd$DRBD'/>
+      <target dev='hda' bus='virtio'/>
+      <alias name='virtio-disk0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
+    </disk>
+    <interface type='bridge'>
+      <mac address='52:54:00:38:81:$DRBDPORT'/>
+      <source bridge='br0'/>
+      <target dev='vnet1'/>
+      <model type='virtio'/>
+      <alias name='net0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+    </interface>
+    <input type='mouse' bus='ps2'/>
+    <graphics type='vnc' port='59$DRBDPORT' autoport='no' listen='0.0.0.0'/>
+    <video>
+      <model type='cirrus' vram='9216' heads='1'/>
+      <alias name='video0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+    </video>
+    <memballoon model='virtio'>
+      <alias name='balloon0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
+    </memballoon>
+  </devices>
+</domain>
+EOF
+
+for virshinstance in $VIRSHINSTANCES; do
+  virsh -c $virshinstance define $DOMFILE
+done
+rm $DOMFILE
+
+cat <<EOF | crm configure
+primitive res_VirtualDomain_kvm_$KVMNAME ocf:heartbeat:VirtualDomain \
+        params config="/etc/libvirt/qemu/$KVMNAME.xml" \
+        op start interval="0" timeout="90" \
+        op stop interval="0" timeout="90" \
+        op monitor interval="10" timeout="30" start-delay="0" \
+        op migrate_from interval="0" timeout="60" \
+        op migrate_to interval="0" timeout="120" \
+        meta target-role="stopped"
+        meta allow-migrate="true"
+colocation col_res_VirtualDomain_kvm_$KVMNAME-ms_$KVMNAME inf: res_VirtualDomain_kvm_$KVMNAME ms_$KVMNAME:Master
+order ord_ms_$KVMNAME-res_VirtualDomain_kvm_$KVMNAME inf: ms_$KVMNAME:promote res_VirtualDomain_kvm_$KVMNAME:start
+commit
+EOF
+
+echo "Dinge tun, z.B. Bootmedium auswaehlen"
+
---
+++ usr/local/bin/vserver-neu.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+[ -e /etc/cluster-tools-lihas.conf ] && . /etc/cluster-tools-lihas.conf
+
+if [ ga$4 == ga ]; then
+  echo "usage: $0 VSNAME IP SIZE CONTEXT [DRBDNUM]" >&2
+  echo "usage: $0 test01 10.0.0.115 10G 115" >&2
+  echo "usage: $0 test01 10.0.0.115 10G 115 3" >&2
+  exit 1
+fi
+
+VSNAME=$1
+IP=$2
+SIZE=$3
+CONTEXT=$4
+
+if [ ga$5 == ga ]; then
+  # Naechstes freies DRBD
+  DRBD=$(($(awk '$1 ~ /^device$/ && $2 ~ /^\/dev\/drbd/ {gsub(";","",$2); gsub("/dev/drbd","",$2); print $2}' /etc/drbd.d/*.res | sort -un| tail -1)+1))
+else
+  DRBD=$5
+fi
+
+DRBDPORT=$(printf "%02i" $DRBD)
+
+DUMMY=10
+
+lvcreate -L$SIZE -n vs_$VSNAME vg_$HOST1
+ssh $HOST2 lvcreate -L$SIZE -n vs_$VSNAME vg_$HOST2
+
+cat <<-EOF > /etc/drbd.d/vs_$VSNAME.res
+resource vs_$VSNAME {  
+        protocol        C;
+        syncer {
+                rate    2000M;
+        }
+        on $HOST1 {
+                device          /dev/drbd$DRBD;
+                disk            /dev/$VG1/vs_$VSNAME;
+                flexible-meta-disk      internal;
+                address         $IP_DRBD1:77$DRBDPORT;
+        }
+        on $HOST2 {
+                device          /dev/drbd$DRBD;
+                disk            /dev/$VG2/vs_$VSNAME;
+                flexible-meta-disk      internal;
+                address         $IP_DRBD2:77$DRBDPORT;
+        }
+}
+EOF
+
+rsync -rlHpogDtSvx /etc/drbd.d $HOST2:/etc/
+yes yes | drbdadm create-md vs_$VSNAME
+drbdadm up vs_$VSNAME
+ssh $HOST2 "(yes yes | drbdadm create-md vs_$VSNAME && drbdadm up vs_$VSNAME)"
+
+drbdadm -- -o primary vs_$VSNAME
+
+mkfs.ext4 -L vs_$VSNAME /dev/drbd$DRBD
+mount /dev/drbd$DRBD /mnt
+vserver $VSNAME build --context $CONTEXT --interface $VSNAME=$IF_LAN:$IP/$IF_LAN_NM --hostname $VSNAME -m rsync -- -d $DEBIANDIST --source $VSERVER_TEMPLATE
+cat $VSERVER_TEMPLATE/etc/apt/sources.list > $VSERVER_BASE/$VSNAME/etc/apt/sources.list
+sed -i '/tmpfs/d' /etc/vservers/$VSNAME/fstab
+touch /etc/vservers/$VSNAME/interfaces/0/nodev
+mv $VSERVER_BASE/$VSNAME/* /mnt/
+umount /mnt 
+
+ssh $HOST2 mkdir $VSERVER_BASE/$VSNAME
+
+rsync -rlHpogDtSvx /etc/vservers $HOST2:/etc/
+
+cat <<-EOF | cibadmin -M -p
+  <configuration>
+    <resources>
+      <master id="ms_$VSNAME">
+        <meta_attributes id="ms_$VSNAME-meta_attributes">
+          <nvpair id="ms_$VSNAME-meta_attributes-clone-max" name="clone-max" value="2"/>
+          <nvpair id="ms_$VSNAME-meta_attributes-notify" name="notify" value="true"/>
+        </meta_attributes>
+        <primitive id="res_drbd_drbd$DRBD_$VSNAME" class="ocf" provider="linbit" type="drbd">
+          <instance_attributes id="res_drbd_drbd$DRBD_$VSNAME-instance_attributes">
+            <nvpair id="nvpair-res_drbd_drbd$DRBD_$VSNAME-drbd_resource" name="drbd_resource" value="vs_$VSNAME"/>
+          </instance_attributes>
+          <operations id="res_drbd_drbd$DRBD_$VSNAME-operations">
+            <op interval="0" id="op-res_drbd_drbd$DRBD_$VSNAME-start" name="start" timeout="240"/>
+            <op interval="0" id="op-res_drbd_drbd$DRBD_$VSNAME-promote" name="promote" timeout="90"/>
+            <op interval="0" id="op-res_drbd_drbd$DRBD_$VSNAME-demote" name="demote" timeout="90"/>
+            <op interval="0" id="op-res_drbd_drbd$DRBD_$VSNAME-stop" name="stop" timeout="100"/>
+            <op id="op-res_drbd_drbd$DRBD_$VSNAME-monitor" name="monitor" interval="10" timeout="20" start-delay="1min"/>
+            <op interval="0" id="op-res_drbd_drbd$DRBD_$VSNAME-notify" name="notify" timeout="90"/>
+          </operations>
+          <meta_attributes id="res_drbd_drbd$DRBD_$VSNAME-meta_attributes">
+            <nvpair id="res_drbd_drbd$DRBD_$VSNAME-meta_attributes-is-managed" name="is-managed" value="true"/>
+          </meta_attributes>
+        </primitive>
+      </master>
+      <group id="grp_$VSNAME">
+        <meta_attributes id="grp_$VSNAME-meta_attributes"/>
+        <primitive id="res_fs_$VSNAME" class="ocf" provider="heartbeat" type="Filesystem">
+          <instance_attributes id="res_fs_$VSNAME-instance_attributes">
+            <nvpair id="nvpair-res_fs_$VSNAME-device" name="device" value="/dev/drbd$DRBD"/>
+            <nvpair id="nvpair-res_fs_$VSNAME-directory" name="directory" value="$VSERVER_BASE/$VSNAME"/>
+            <nvpair id="nvpair-res_fs_$VSNAME-fstype" name="fstype" value="ext4"/>
+            <nvpair id="nvpair-res_fs_$VSNAME-options" name="options" value="noatime,stripe=64,barrier=0"/>
+          </instance_attributes>
+          <operations id="res_fs_$VSNAME-operations">
+            <op interval="0" id="op-res_fs_$VSNAME-start" name="start" timeout="600"/>
+            <op interval="0" id="op-res_fs_$VSNAME-stop" name="stop" timeout="60"/>
+            <op id="op-res_fs_$VSNAME-monitor" name="monitor" interval="20" timeout="40" start-delay="0"/>
+            <op interval="0" id="op-res_fs_$VSNAME-notify" name="notify" timeout="60"/>
+          </operations>
+          <meta_attributes id="res_fs_$VSNAME-meta_attributes">
+            <nvpair id="res_fs_$VSNAME-meta_attributes-is-managed" name="is-managed" value="true"/>
+            <nvpair id="res_fs_$VSNAME-meta_attributes-target-role" name="target-role" value="started"/>
+          </meta_attributes>
+        </primitive>
+        <primitive id="res_IPaddr2_ip_$VSNAME" class="ocf" provider="heartbeat" type="IPaddr2">
+          <instance_attributes id="res_IPaddr2_ip_$VSNAME-instance_attributes">
+            <nvpair id="nvpair-res_IPaddr2_ip_$VSNAME-ip" name="ip" value="$IP"/>
+            <nvpair id="nvpair-res_IPaddr2_ip_$VSNAME-nic" name="nic" value="br0"/>
+            <nvpair id="nvpair-res_IPaddr2_ip_$VSNAME-cidr_netmask" name="cidr_netmask" value="16"/>
+            <nvpair id="nvpair-res_IPaddr2_ip_$VSNAME-broadcast" name="broadcast" value="10.0.255.255"/>
+            <nvpair id="nvpair-res_IPaddr2_ip_$VSNAME-iflabel" name="iflabel" value="$VSNAME"/>
+          </instance_attributes>
+          <operations id="res_IPaddr2_ip_$VSNAME-operations">
+            <op interval="0" id="op-res_IPaddr2_ip_$VSNAME-start" name="start" timeout="20"/>
+            <op interval="0" id="op-res_IPaddr2_ip_$VSNAME-stop" name="stop" timeout="20"/>
+            <op id="op-res_IPaddr2_ip_$VSNAME-monitor" name="monitor" interval="10" timeout="20" start-delay="0"/>
+          </operations>
+          <meta_attributes id="res_IPaddr2_ip_$VSNAME-meta_attributes">
+            <nvpair id="res_IPaddr2_ip_$VSNAME-meta_attributes-target-role" name="target-role" value="started"/>
+          </meta_attributes>
+        </primitive>
+        <primitive id="res_VServer-lihas_vs_$VSNAME" class="ocf" provider="lihas" type="VServer-lihas">
+          <instance_attributes id="res_VServer-lihas_vs_$VSNAME-instance_attributes">
+            <nvpair id="nvpair-res_VServer-lihas_vs_$VSNAME-vservername" name="vservername" value="$VSNAME"/>
+          </instance_attributes>
+          <operations id="res_VServer-lihas_vs_$VSNAME-operations">
+            <op interval="0" id="op-res_VServer-lihas_vs_$VSNAME-start" name="start" timeout="600"/>
+            <op interval="0" id="op-res_VServer-lihas_vs_$VSNAME-stop" name="stop" timeout="180"/>
+            <op id="op-res_VServer-lihas_vs_$VSNAME-monitor" name="monitor" interval="10" timeout="20" start-delay="5"/>
+          </operations>
+          <meta_attributes id="res_VServer-lihas_vs_$VSNAME-meta_attributes">
+            <nvpair id="res_VServer-lihas_vs_$VSNAME-meta_attributes-is-managed" name="is-managed" value="true"/>
+            <nvpair id="res_VServer-lihas_vs_$VSNAME-meta_attributes-target-role" name="target-role" value="started"/>
+          </meta_attributes>
+        </primitive>
+      </group>
+    </resources>
+    <constraints>
+      <rsc_location id="loc_ms_$VSNAME-$HOST1" rsc="ms_$VSNAME" node="$HOST1" score="2"/>
+      <rsc_location id="loc_ms_$VSNAME-$HOST2" rsc="ms_$VSNAME" node="$HOST2" score="0"/>
+      <rsc_colocation id="col_res_fs_$VSNAME-ms_$VSNAME" score="INFINITY" with-rsc-role="Master" rsc="res_fs_$VSNAME" with-rsc="ms_$VSNAME"/>
+      <rsc_order id="ord_ms_$VSNAME-res_fs_$VSNAME" score="INFINITY" first-action="promote" then-action="start" first="ms_$VSNAME" then="res_fs_$VSNAME"/>
+    </constraints>
+  </configuration>
+EOF
